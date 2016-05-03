dc33f87e86241d3798b0127e4de37587081fa4a6
jiale.chan@gmail.com <jiale.chan@gmail.com>
2016-05-03 17:41:52 +0800

    jtool-apiclient 调整下代码结构

--- src/main/java/com/jtool/apiclient/ApiClient.java
+++ src/main/java/com/jtool/apiclient/ApiClient.java
@@ -6,14 +6,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.io.*;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-
-import static com.jtool.apiclient.Util.*;
 
 public class ApiClient {
 
@@ -25,13 +28,13 @@ public class ApiClient {
 
     public static class Request {
 
-        private Request() {
-        }
-
         private Map<String, String> header;
         private Map<String, Object> param;
         private String _logId;
         private String url;
+
+        private Request() {
+        }
 
         public Request header(Map<String, String> header) {
             //header方法应该只调用一次
@@ -51,7 +54,7 @@ public class ApiClient {
             if (param instanceof Map) {
                 this.param = (Map) param;
             } else {
-                this.param = Util.bean2Map(param);
+                this.param = bean2Map(param);
             }
 
             return this;
@@ -103,7 +106,7 @@ public class ApiClient {
         Map<String, String> header = request.getHeader();
         Map<String, Object> params = request.getParam();
 
-        String paramsString = Util.params2paramsStr(params);
+        String paramsString = params2paramsStr(params);
 
         if (!"".equals(paramsString)) {
             if (urlStr.contains("?")) {
@@ -126,7 +129,7 @@ public class ApiClient {
 
             int responseCode = httpURLConnection.getResponseCode();
             if (responseCode == 200) {
-                result = Util.readAndCloseStream(httpURLConnection.getInputStream());
+                result = readAndCloseStream(httpURLConnection.getInputStream());
             } else {
                 logHttpURLConnectionErrorStream(httpURLConnection);
                 throw new StatusCodeNot200Exception(urlStr, params, responseCode);
@@ -153,7 +156,7 @@ public class ApiClient {
         Map<String, String> header = request.getHeader();
         Map<String, Object> params = request.getParam();
 
-        String paramsString = Util.params2paramsStr(params);
+        String paramsString = params2paramsStr(params);
 
         log.debug("发送请求: curl '" + urlStr + "' " + makeHeaderLogString(header) + " -X POST -d '" + paramsString + "'");
 
@@ -197,7 +200,7 @@ public class ApiClient {
 
             int responseCode = httpURLConnection.getResponseCode();
             if (responseCode == 200) {
-                result = Util.readAndCloseStream(httpURLConnection.getInputStream());
+                result = readAndCloseStream(httpURLConnection.getInputStream());
             } else {
                 logHttpURLConnectionErrorStream(httpURLConnection);
                 throw new StatusCodeNot200Exception(urlStr, params, responseCode);
@@ -314,7 +317,7 @@ public class ApiClient {
 
             int responseCode = httpURLConnection.getResponseCode();
             if (responseCode == 200) {
-                result = Util.readAndCloseStream(httpURLConnection.getInputStream());
+                result = readAndCloseStream(httpURLConnection.getInputStream());
             } else {
                 logHttpURLConnectionErrorStream(httpURLConnection);
                 throw new StatusCodeNot200Exception(url, params, responseCode);
@@ -332,5 +335,145 @@ public class ApiClient {
         log.debug("请求返回: " + result);
 
         return result;
+    }
+
+    private static String readAndCloseStream(InputStream is) throws IOException {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(is);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        try {
+            while ((len = bufferedInputStream.read(buffer)) != -1) {
+                os.write(buffer, 0, len);
+            }
+            return os.toString();
+        } finally {
+            if(os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    os = null;
+                }
+            }
+            if(is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    private static String params2paramsStr(Map<String, Object> params) {
+        String paramsString = "";
+        if(params != null && params.size() > 0) {
+            for(String paramName : params.keySet()) {
+                if(params.get(paramName) != null) {
+                    if (!"".equals(paramsString)) {
+                        paramsString += "&";
+                    }
+                    try {
+                        paramsString += URLEncoder.encode(paramName, "UTF-8") + "=" + URLEncoder.encode(params.get(paramName).toString(), "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return paramsString;
+    }
+
+    private static Map<String, Object> bean2Map(Object obj) {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        if(obj == null){
+            return map;
+        }
+
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            for (PropertyDescriptor property : propertyDescriptors) {
+                String key = property.getName();
+
+                // 过滤class属性
+                if (!"class".equals(key)) {
+                    // 得到property对应的getter方法
+                    Method getter = property.getReadMethod();
+                    Object value = getter.invoke(obj);
+
+                    if(value != null) {
+                        map.put(key, value);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.debug("将bean转化为map时发生错误：" + obj);
+        }
+
+        return map;
+    }
+
+    private static void addHeaderToHttpURLConnection(Map<String, String> header, HttpURLConnection httpURLConnection) {
+        if(header != null) {
+            for (String key : header.keySet()) {
+                httpURLConnection.setRequestProperty(key, header.get(key));
+            }
+        }
+    }
+
+    private static boolean isPostFile(Map<String, Object> params) {
+        if(params == null) {
+            return false;
+        }
+        for (String key : params.keySet()) {
+            Object param = params.get(key);
+            if (param instanceof File) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String makeHeaderLogString(Map<String, String> header) {
+        String headerStr = "";
+        if(header != null) {
+            for(String headerKey : header.keySet()) {
+                headerStr += " -H '" + headerKey + ": " + header.get(headerKey) + "' ";
+            }
+        }
+        return headerStr;
+    }
+
+    private static void logHttpURLConnectionErrorStream(HttpURLConnection httpURLConnection) {
+        try {
+            if(httpURLConnection != null) {
+                String errorStream = readAndCloseStream(httpURLConnection.getErrorStream());
+                log.error("访问发生异常，错误码是：" + httpURLConnection.getResponseCode() + "\t错误流信息为：" + errorStream);
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static String getContentTypeByFilename(String filename) {
+
+        int index = filename.lastIndexOf(".");
+
+        if(index == -1) {
+            return "application/octet-stream";
+        }
+
+        String suffix = filename.substring(index);
+
+        if(".gif".equalsIgnoreCase(suffix)) {
+            return "image/gif";
+        } else if(".jpg".equalsIgnoreCase(suffix) || ".jpeg".equalsIgnoreCase(suffix)){
+            return "image/jpeg";
+        } else if(".png".equalsIgnoreCase(suffix)) {
+            return "image/png";
+        } else {
+            return "application/octet-stream";
+        }
     }
 }
