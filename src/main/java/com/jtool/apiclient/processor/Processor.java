c402abeb3ccf9cee377b52fcbe85fee1effb8fbf
jiale.chan@gmail.com <jiale.chan@gmail.com>
2017-10-14 14:26:23 +0800

    0.0.13版本 增加head 方法

--- src/main/java/com/jtool/apiclient/processor/Processor.java
+++ src/main/java/com/jtool/apiclient/processor/Processor.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
 
 import static com.jtool.apiclient.Util.*;
 
