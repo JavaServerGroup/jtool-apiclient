381a81920d24235831a09512b45d9ef9779aea28
jialechan <jiale.chen@afmobigroup.com>
2017-11-14 15:12:38 +0800

    更加详细的测试日志

--- src/test/java/com/jtool/apiclient/demo/ApiClientTest.java
+++ src/test/java/com/jtool/apiclient/demo/ApiClientTest.java
@@ -23,7 +23,7 @@ import static com.jtool.apiclient.ApiClient.Api;
 @SpringBootApplication
 public class ApiClientTest {
 
-    private final String host = "http://localhost:8080";
+    private final String host = "http://localhost:8082";
 
     @BeforeClass
     public static void setup() {
